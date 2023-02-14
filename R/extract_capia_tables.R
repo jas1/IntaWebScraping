d33d0b105813640c94b2ef354a5018b12db0484d
Julio Spairani <jspairani@gmail.com>
2023-02-14 07:33:50 -0300

    fix scrapping capia
    los datos que vinieron de capia estan malos para algunas fechas, hubo que cambiar el procesamiento de extracción de la información

--- R/extract_capia_tables.R
+++ R/extract_capia_tables.R
@@ -10,14 +10,53 @@ extract_capia_tables <- function(dl_html_file){
 
   downloaded_file <- rvest::read_html(dl_html_file)
 
-  obtener_tablas <- downloaded_file|>
-    # rvest::html_elements("#sp-main-body")
-    rvest::html_elements(".post_intro table") |>
-    rvest::html_table() |>
-    purrr::map_dfr(.f=function(df_x){
+  #  para obtener bien los titulos de fechas publicadas: #
+  tmp_posts_titulos <- downloaded_file |> rvest::html_elements(".post_intro h2 a") |> rvest::html_text() |> stringr::str_trim()
+# tmp_x <- downloaded_file |> rvest::html_elements(".post_intro")
+  posts <- downloaded_file |> rvest::html_elements(".post_intro") |>  # cantidad de posts
+  purrr::map(.f=function(x){
+    # x <- tmp_x[[2]]
+     extraer_titulos <- x |> rvest::html_elements("h2 a") |> rvest::html_text() |> stringr::str_trim()
+
+    extraer_tablas <- x |> rvest::html_elements("table") |> rvest::html_table()
+    if(length(extraer_tablas)!=1){
+      warning(glue::glue("PROBABLE ERROR EN DATOS DE TABLA: extract_capia_tables, tabla != de 1.\n
+              REVISAR pagina de capia para el post de fecha: '{extraer_titulos}', poniendo tabla vacia.\n
+              Cargar datos tablas 'carne_ave' y 'huevos' MANUALMENTE."))
+      extraer_tablas <- list( tibble::tibble(X1=character(),X2=character(),X3=character(),X4=character(),X5=character()) )
+    }
+
+    tibble::tibble(titulo=extraer_titulos,tabla=extraer_tablas)
+  }) |>
+    purrr::reduce(dplyr::union)
+  # tema de tablas: ej el 06/02: publicaron un print screen de un excel !
+  # si cantidad de tablas != cantidad de posts => seguro publicaron algo diferente de tabla.
+  # en ese caso van a tener que cargarlo a mano.
+
+  tablas_procesadas <- posts |>
+    dplyr::mutate(procesadas = purrr::map2(.x=tabla,.y = titulo,.f=function(df_x,titulo_x){
+      # idx_x <- 1
+      # titulo_x <- posts$titulo[[idx_x]]
+      # df_x <- posts$tabla[[idx_x]]
       ref_semana <- df_x |>
         dplyr::filter(stringr::str_detect(stringr::str_to_upper(X1),"REFERENCIA SEMANA")) |>
         dplyr::pull(X1)
+      if (nrow(df_x)==0) {
+        warning(glue::glue("PROBABLE ERROR EN DATOS DE TABLA: extract_capia_tables, tabla vacia. \n
+              REVISAR pagina de CAPIA para el post de fecha: '{titulo_x}', utilizando el titulo del post en vez de referencia semana.
+               Cargar datos tablas 'carne_ave' y 'huevos' MANUALMENTE. \n
+              "))
+        ref_semana <- titulo_x
+      }else{
+        if (length(ref_semana)!=1) {
+          warning(glue::glue("PROBABLE ERROR EN DATOS DE TABLA: extract_capia_tables, fila: 'REFERENCIA SEMANA' != de 1. \n
+              REVISAR pagina de CAPIA para el post de fecha: '{titulo_x}', utilizando el titulo del post en vez de referencia semana.
+              REVISAR carga de datos tabla carne_ave y huevos. Probable necesidad de corrección de forma manual. \n
+              "))
+          ref_semana <- titulo_x
+        }
+      }
+
 
       ref_huevo <- df_x |>
         dplyr::filter(stringr::str_detect(stringr::str_to_lower(X1),"polla recriada de 16 semanas blanca")) |>
@@ -35,7 +74,8 @@ extract_capia_tables <- function(dl_html_file){
 
       resu <- ref_huevo |> dplyr::union(ref_carne_ave)
       resu
-    })
+    }))
+  resultado <- tablas_procesadas |> dplyr::select(procesadas)|> tidyr::unnest(procesadas)
 
-  obtener_tablas
+  resultado
 }
